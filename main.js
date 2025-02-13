22d1604ff3eb273c16bcc8a067415c0fc24d527a
thomasthomsen16 <35918236+thomasthomsen16@users.noreply.github.com>
2025-02-13 12:04:39 +0100

    Set static charts bg color to white with 0.5 opacity

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ function renderStaticChart(sampledData, chartId) {
         height: 600,
         data: { values: sampledData },
         mark: { type: "circle" },
-        config: { background: "#9494948a" },
+        config: { background: "#ffffff80"},
         encoding: {
             x: { field: "danceability", type: "quantitative", scale: { domain: [0, 1] } },
             y: { field: "tempo", type: "quantitative", scale: { domain: [0, 220] } },
@@ -153,7 +153,7 @@ function renderStaticChart2(sampledData, chartId) {
         height: 600,
         data: { values: sampledData },
         mark: { type: "circle" },
-        config: { background: "#9494948a" },
+        config: { background: "#ffffff80" },
         encoding: {
             x: { field: "valence", type: "quantitative", scale: { domain: [0, 1] } },
             y: { field: "energy", type: "quantitative", scale: { domain: [0, 1] } },
